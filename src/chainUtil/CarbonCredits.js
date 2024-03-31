242caf4f9c6d8872a2dc8951252b0792e9d53f66
slc <1084884210@qq.com>
2024-03-31 13:14:40 +0800

    扣1送地狱火

--- src/chainUtil/CarbonCredits.js
+++ src/chainUtil/CarbonCredits.js
@@ -1,5 +1,6 @@
 import {Web3} from 'web3'
 const App = {
+    web3: null,
     web3Provider: null,
     contracts: {},
 
@@ -21,26 +22,287 @@ const App = {
         else {
             App.web3Provider = new Web3.providers.HttpProvider('http://119.23.143.76:8545')
         }
-        let web3 = new Web3(App.web3Provider)
-        console.log(web3.eth.accounts[0])
-        web3.eth.defaultAccount = "0x96a0cfE920bF1CcD1ef1cAe4b9592C41334CaC81"
+        App.web3 = new Web3(App.web3Provider)
+        console.log(App.web3.eth.accounts[0])
+        App.web3.eth.defaultAccount = App.web3.eth.accounts[0]
         return App.initContract()
     },
     initContract: function() {
-        $.getJSON('src/assets/static/CarbonCredits.json', function(data){
-            App.contracts.InfoContract = TruffleContract(data);
-            App.contracts.InfoContract.setProvider(App.web3Provider);
-            App.getInfo();
-        });
+        App.contracts.cc_Contract = new App.web3.eth.Contract(abi, address)
+        App.getInfo()
     },
     getInfo: function() {
-        App.contracts.InfoContract.deployed().then(function(instance) {
-            return instance.allowanceOf(web3.eth.accounts[0],{gas: 500000});
-        }).then(function(result) {
-            console.log(result);
-        }).catch(function(err) {
-            console.error(err);
-        });
+        App.contracts.cc_Contract.methods.allowanceOf("0x96a0cfE920bF1CcD1ef1cAe4b9592C41334CaC81").call((err,res)=>{
+            console.log(res)
+        })
     }
 }
+const abi = [
+    {
+        "inputs": [
+            {
+                "internalType": "contract IERC20",
+                "name": "_carbonCoin",
+                "type": "address"
+            },
+            {
+                "internalType": "address",
+                "name": "_carbonIssuer",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": false,
+                "internalType": "address",
+                "name": "account",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "internalType": "address",
+                "name": "target",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "internalType": "uint256",
+                "name": "amount",
+                "type": "uint256"
+            }
+        ],
+        "name": "CarbonAllowanceIssued",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": false,
+                "internalType": "address",
+                "name": "account",
+                "type": "address"
+            },
+            {
+                "components": [
+                    {
+                        "internalType": "string",
+                        "name": "reportText",
+                        "type": "string"
+                    }
+                ],
+                "indexed": false,
+                "internalType": "struct CarbonCredits.CarbonReport",
+                "name": "carbonReport",
+                "type": "tuple"
+            }
+        ],
+        "name": "CarbonReportSubmitted",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": false,
+                "internalType": "address",
+                "name": "_from",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "internalType": "address",
+                "name": "_to",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "internalType": "uint256",
+                "name": "_amount",
+                "type": "uint256"
+            },
+            {
+                "indexed": false,
+                "internalType": "uint256",
+                "name": "_price",
+                "type": "uint256"
+            }
+        ],
+        "name": "CarbonTransaction",
+        "type": "event"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "name": "carbonAllowance",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function",
+        "constant": true
+    },
+    {
+        "inputs": [],
+        "name": "carbonCoin",
+        "outputs": [
+            {
+                "internalType": "contract IERC20",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function",
+        "constant": true
+    },
+    {
+        "inputs": [],
+        "name": "carbonIssuer",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function",
+        "constant": true
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "name": "carbonReports",
+        "outputs": [
+            {
+                "internalType": "string",
+                "name": "reportText",
+                "type": "string"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function",
+        "constant": true
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "account",
+                "type": "address"
+            },
+            {
+                "internalType": "address",
+                "name": "target",
+                "type": "address"
+            },
+            {
+                "internalType": "uint256",
+                "name": "amount",
+                "type": "uint256"
+            }
+        ],
+        "name": "issueCarbonAllowance",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "_to",
+                "type": "address"
+            },
+            {
+                "internalType": "uint256",
+                "name": "_amount",
+                "type": "uint256"
+            },
+            {
+                "internalType": "uint256",
+                "name": "_price",
+                "type": "uint256"
+            }
+        ],
+        "name": "carbonTransaction",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "string",
+                "name": "report",
+                "type": "string"
+            }
+        ],
+        "name": "submitCarbonReport",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "account",
+                "type": "address"
+            }
+        ],
+        "name": "allowanceOf",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function",
+        "constant": true
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "account",
+                "type": "address"
+            }
+        ],
+        "name": "reportOf",
+        "outputs": [
+            {
+                "internalType": "string",
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function",
+        "constant": true
+    }
+]
+const address = "0x7E9669D0087Bb44Effba97D7EEc18eC258962dB8"
 export default App;
