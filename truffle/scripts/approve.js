da58da82f372bb08543616bded15800544786b15
Harmony_Quan <3102676090@qq.com>
2024-04-12 15:10:24 +0800

    泵

--- truffle/scripts/approve.js
+++ truffle/scripts/approve.js
@@ -6,7 +6,7 @@ const CarbonCredits = artifacts.require("CarbonCredits")
 
 const accounts = ["0x90E672df02F2aed9A98a16324e6eE00344f6c7C3", // 0 系统
                   "0x46d9Be20C17CA09e09efC293f8e0BFc3B3efFb22", // 1 数据审核员
-                  "0x2f875a0xd078d8bEADc71ae788f13F79F4CA6346DF2400CE7c2069a7b389c24e6227755cde6494e56d"] // 2 第三方机构
+                  "0xd078d8bEADc71ae788f13F79F4CA6346DF2400CE"] // 2 第三方机构
 
 module.exports = async function (callback) {
     const carbonCoin = await CarbonCoin.deployed()
